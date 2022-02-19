c2663b766a793d5773e2abf3ecfcebcd0d5e78a0
Jose Herrera <davidjxd32@hotmail.com>
2022-02-18 18:34:00 -0600

    Add project files.

---
+++ LabConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
